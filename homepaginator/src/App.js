27b7dbabc98dc588ff05d145e865121b6c91b198
Themba Mndebele <Mndebele.themba@outlook.com>
2021-02-23 23:19:06 +0200

    Added basic search and basic folder view

--- homepaginator/src/App.js
+++ homepaginator/src/App.js
@@ -1,27 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+
+//import logo from './logo.svg';
+import RandomID from './common';
 import './App.css';
 import { Search } from './search/Search.js';
+import { Folders } from './bookmark_folders/Folders.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloads dawg.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Search/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialFolders: _fakeFolders,
+      folders: _fakeFolders,
+      searchTerm: '',
+    }
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+  }
+
+  handleSearchChange(e) {
+    this.setState({searchTerm: e.target.value})
+    let folders = this.state.initialFolders.slice();
+
+    if (e.target.value) {
+      let matches = folders.filter(c => c.title.toLowerCase().includes(e.target.value.toLowerCase()));
+      folders = matches;
+    }
+
+     this.setState({folders: folders});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Search searchTerm={this.state.searchTerm}
+                handleSearchChange={(e) => this.handleSearchChange(e)}/>
+        <Folders items={this.state.folders}/>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+//Fake data for the time-being
+let _fakeFolders = [
+  { id: RandomID(), title: 'Design', count: 47 },
+  { id: RandomID(), title: 'Develop', count: 28 },
+  { id: RandomID(), title: 'My bookmarks', count: 103 },
+  { id: RandomID(), title: 'Anime', count: 93 }
+]
